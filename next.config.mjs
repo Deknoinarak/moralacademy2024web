b438defcb0716f297867b5f6b0464705f255bf3a
Nonthapat Jamjai <chitah09@gmail.com>
2024-05-02 13:46:28 +0700

    Fix: pdf icon

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
         port: "",
         pathname: "/images/**",
       },
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
