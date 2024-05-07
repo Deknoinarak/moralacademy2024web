5c6dfa528308064c050c12992a317fa501b8a85c
Nonthapat Jamjai <chitah09@gmail.com>
2024-05-07 21:47:53 +0700

    Fix: Carousel Button To Fit

--- src/views/index.php
+++ src/views/index.php
@@ -134,7 +134,7 @@
         </div>
       </div>
       <!-- Slider controls, if desired -->
-      <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+      <button type="button" class="absolute top-1/2 transform -translate-y-1/2 left-0 z-30 flex items-center justify-center px-4 focus:outline-none group" data-carousel-prev>
         <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none transition-all ease-in-out">
           <svg aria-hidden="true" class="w-5 h-5 text-zinc-400 hover:text-zinc-800 sm:w-6 sm:h-6 dark:text-gray-800 transition-all ease-in-out" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
@@ -142,7 +142,7 @@
           <span class="sr-only">Previous</span>
         </span>
       </button>
-      <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+      <button type="button" class="absolute top-1/2 transform -translate-y-1/2 right-0 z-30 flex items-center justify-center px-4 focus:outline-none group" data-carousel-next>
         <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none transition-all ease-in-out">
           <svg aria-hidden="true" class="w-5 h-5 text-zinc-400 hover:text-zinc-800 sm:w-6 sm:h-6 dark:text-gray-800 transition-all ease-in-out" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
